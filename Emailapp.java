532d073bb972dbac333834b7dd90ac3c27283642
Mathewdoe <thandazani.matthew@gmail.com>
2021-02-15 10:42:30 +0200

    Initial Commit

---
+++ Emailapp.java
@@ -0,0 +1,8 @@
+public class Emailapp {
+
+    public static void main(String[] args) {
+        Email em1 = new Email("Matthew", "Dube");
+        System.out.println(em1.showInfo());   
+    }
+    
+}
